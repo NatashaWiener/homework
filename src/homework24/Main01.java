20e1809dfb9de21bf897eb63ff0124e0418db19d
NatashaWiener <149413843+NatashaWiener@users.noreply.github.com>
2023-11-05 13:02:07 +0100

    change

--- src/homework24/Main01.java
+++ src/homework24/Main01.java
@@ -1,2 +1,10 @@
-package homework24;public class Main01 {
+package homework24;
+
+public class Main01 {
+
+  public static void main(String[] args) {
+    System.out.println("Hello word");
+
+  }
+
 }
