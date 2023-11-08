5dbdf21aba466b7c8cff5c18312afb9d91ab5cab
NatashaWiener <149413843+NatashaWiener@users.noreply.github.com>
2023-11-08 09:49:05 +0100

    homework25

--- src/homework25/human/Main.java
+++ src/homework25/human/Main.java
@@ -1,2 +1,22 @@
-package homework25.human;public class Main {
+package homework25.human;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Human Fred = new Human();
+    Fred.name = "Fred";
+    Fred.age = 36;
+    Human Anna = new Human();
+    Anna.name = "Anna";
+    Anna.age = 30;
+    Human Stella = new Human();
+    Stella.name = "Stella";
+    Stella.age = 28;
+
+    Fred.introduce();
+    Anna.introduce();
+    Stella.introduce();
+  }
+
+
 }
