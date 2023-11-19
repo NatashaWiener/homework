9e6a97d1f4713da7ba69835e502f861c13719234
NatashaWiener <149413843+NatashaWiener@users.noreply.github.com>
2023-11-19 14:16:26 +0100

    homework30 main01

---
+++ src/homework30/Main01.java
@@ -0,0 +1,23 @@
+package homework30;
+
+public class Main01 {
+    public static void main(String[] args) {
+        /* Целочисленные типы данных:
+        byte,short,int,long
+        нецелые типы данных:
+        float,double
+        символные типы данных:
+        char,String
+        Логический тип данных:
+      char,boolean */
+        short num = 6893;
+        float num1 = 67.58f;
+        char letter = 'm';
+        boolean flag = true;
+        System.out.println("Целый: " + num + " - 4 байт");
+        System.out.println("Нецелый: " + num1 + " - 8 байт");
+        System.out.println("Символьный: " + letter + " - 1 байт");
+        System.out.println("Логический: " + flag + " - 1 байт");
+
+    }
+}
