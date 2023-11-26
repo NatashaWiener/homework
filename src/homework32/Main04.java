b05fce4d6a09652c0c2d1fb57f5e6d5c37df119d
NatashaWiener <149413843+NatashaWiener@users.noreply.github.com>
2023-11-26 12:18:35 +0100

    main04

--- src/homework32/Main04.java
+++ src/homework32/Main04.java
@@ -1,3 +1,32 @@
 package homework32;
+
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main04 {
-}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите размер масива: ");
+        int size = scanner.nextInt();
+
+        System.out.println("Введите масив целых чисел: ");
+        int[] arr = new int[scanner.nextInt()];
+        for (int i = 1; i < size; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+
+
+
+           System.out.println(" Масив отсортирован!");
+
+
+           System.out.println(" Масив не отсортирован!");
+        }
+
+
+
+    }
+
+
