afe7604912112111a8cb1a226c88a80a62537056
NatashaWiener <149413843+NatashaWiener@users.noreply.github.com>
2023-11-26 13:45:41 +0100

    main02

---
+++ src/homework33/Main02.java
@@ -0,0 +1,15 @@
+package homework33;
+
+public class Main02 {
+    public static char[] englishAlphabet() {
+        char[] alphaBet = new char[26];
+        for (int i = 0; i < 26; i++) {
+            alphaBet[i] = (char) ('a' + i);
+        }
+        return alphaBet;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
