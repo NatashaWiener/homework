b18870399d51994ff04af3013eee8694a724520e
NatashaWiener <149413843+NatashaWiener@users.noreply.github.com>
2023-11-27 17:55:48 +0100

    change main01

--- src/homework33/Main01.java
+++ src/homework33/Main01.java
@@ -1,5 +1,7 @@
 package homework33;
 
+import java.util.Scanner;
+
 public class Main01 {
     public static void changeSign(int[] arr) {
 
@@ -10,21 +12,13 @@ public class Main01 {
 
     }
 
-    public static void print(int[] arr) {
-        for (int i = 0; i < arr.length ; i++)  {
-            System.out.print(arr[i] + " ");
-        }
-    }
-
     public static void main(String[] args) {
-        int[] arr = {1, 5, 7, -4, 8, -3, -6};
-            System.out.print("Исходный масив: " );
-        print(arr);
 
-        System.out.println();
-        System.out.print("Измененный масив: ");
+        int[] arr = {1, 5, 7, -4, 8, -3, -6};
         changeSign(arr);
 
         }
-    }
+
+        }
+
 
